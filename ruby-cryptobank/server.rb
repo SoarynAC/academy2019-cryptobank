df5d308dff7ca67995fe7515d571ad5b0a6858ca
Murilo Conde <murilo.conde@fatec.sp.gov.br>
2019-08-01 18:57:33 -0300

    updated ruby api

--- ruby-cryptobank/server.rb
+++ ruby-cryptobank/server.rb
@@ -9,6 +9,8 @@ require_relative 'models/account'
 
 Mongoid.load! 'mongoid.yml'
 
+Mongoid.raise_not_found_error = false
+
 # Auth class
 class JwtAuth
   def initialize(app)
@@ -35,23 +37,31 @@ end
 class Public < Sinatra::Base
   post '/create_account' do
     content_type :json
-    account = Account.create(JSON.parse(request.body.read))
-    account.to_json
+
+    account = JSON.parse request.body.read
+    has_account = Account.find_by(email: account['email'])
+
+    if has_account.nil?
+      Account.create(account)
+      { msg: 'Conta criada com sucesso!' }.to_json
+    else
+      halt 400, { 'Content-Type' => 'application/json' },
+           { msg: 'Email já cadastrado!' }.to_json
+    end
   end
 
-  get '/login' do
+  post '/login/:email' do
     email = params[:email]
-    password = params[:password]
+    password = JSON.parse(request.body.read)['password']
 
     account = Account.find_by(email: email, password: password)
 
     if !account.nil?
       content_type :json
-
       { token: token(email) }.to_json
-
     else
-      halt 401
+      halt 401, { 'Content-Type' => 'application/json' },
+           { msg: 'Email ou senha incorretos!' }.to_json
     end
   end
 
@@ -90,6 +100,11 @@ class Api < Sinatra::Base
     user = request.env[:user]
     email = user['email'].to_sym
 
+    unless params['amount']
+      halt 400, { 'Content-Type' => 'application/json' },
+           { msg: 'Informe o valor!' }.to_json
+    end
+
     if !Account.find_by(email: email).nil?
       content_type :json
       account = Account.find_by(email: email)
@@ -106,6 +121,11 @@ class Api < Sinatra::Base
     user = request.env[:user]
     email = user['email'].to_sym
 
+    unless params['amount']
+      halt 400, { 'Content-Type' => 'application/json' },
+           { msg: 'Informe o valor!' }.to_json
+    end
+
     if !Account.find_by(email: email).nil?
       content_type :json
       account = Account.find_by(email: email)
@@ -113,6 +133,37 @@ class Api < Sinatra::Base
 
       account.update_attribute(:money, money - params['amount'].to_f)
       { money: account.money }.to_json
+    else
+      halt 403
+    end
+  end
+
+  put '/money/transfer/:email/:amount' do
+    user = request.env[:user]
+    email = user['email'].to_sym
+
+    if !params['email'] || !params['amount']
+      halt 400, { 'Content-Type' => 'application/json' },
+           { msg: 'Informe a conta e o valor!' }.to_json
+    end
+
+    if !Account.find_by(email: email).nil?
+      content_type :json
+
+      account_to = Account.find_by(email: params['email'])
+
+      if !account_to.nil?
+        account_from = Account.find_by(email: email)
+        money_from = account_from.money
+        money_to = account_to.money
+
+        account_from.update_attribute(:money, money_from - params['amount'].to_f)
+        account_to.update_attribute(:money, money_to + params['amount'].to_f)
+        { money_from: account_from.money,
+          money_to: account_to.money }.to_json
+      else
+        halt 400
+      end
     else
       halt 403
     end
