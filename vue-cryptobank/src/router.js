6b9c6781a039cf0f27d4c6ef1c5700d5f3f88b71
Murilo Conde <murilo.conde@fatec.sp.gov.br>
2019-08-02 21:20:06 -0300

    added routes and auth requirement

--- vue-cryptobank/src/router.js
+++ vue-cryptobank/src/router.js
@@ -1,25 +1,78 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+/* eslint-disable no-unused-vars */
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Deposit from "./views/Deposit.vue";
+import Pay from "./views/Pay.vue";
+import Transfer from "./views/Transfer.vue";
+import { http } from "./http";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
+let router = new Router({
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: "/pay",
+      name: "pay",
+      component: Pay,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/transfer",
+      name: "transfer",
+      component: Transfer,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/deposit",
+      name: "deposit",
+      component: Deposit,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/login",
+      name: "about",
+      component: () => import("./views/Login.vue")
+    },
+    {
+      path: "/create_account",
+      name: "create_account",
+      component: () => import("./views/CreateAccount.vue")
     }
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth) {
+    http
+      .post("api/auth")
+      .then(_response => {
+        next();
+      })
+      .catch(_error => {
+        next("login");
+      });
+  } else {
+    next();
+  }
+});
+
+export default router;
